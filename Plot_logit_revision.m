f3bb006edadcf37814ad1dcddcf3a6c81680abf6
Kai Hwang <kai.hwang@gmail.com>
2014-06-18 18:46:28 -0400

    improve legend.

--- Plot_logit_revision.m
+++ Plot_logit_revision.m
@@ -2,7 +2,7 @@
 
 load Adult_logit_revision.mat
 
-figure
+figure;
 
 topedge=-90;
 botedge=90;
@@ -20,21 +20,21 @@ xv=[];
 for i =1:length(binEdges)-1
     xv = [xv,mean(binEdges(i:i+1))];
 end
-hold on
-plot(xv,binMean(1:end),'ok','MarkerSize',15,'LineWidth',2)
-xlim([-60 60])
-ylim([0.6 1])
+hold on;
+h1 = plot(xv,binMean(1:end),'ok','MarkerSize',15,'LineWidth',2);
+xlim([-60 60]);
+ylim([0.6 1]);
 
-clear x y s
+clear x y s;
 x=-60:1:60;
 for s=1:length(x)
     y(s) = (exp( 1.575033+0.006644*x(s)))/(1+exp(1.575033+0.006644*x(s)));
 end
 
-plot(x,y,'-k','LineWidth',2,...
+h2 = plot(x,y,'-k','LineWidth',2,...
     'MarkerEdgeColor','k',...
     'MarkerFaceColor','k',...
-    'MarkerSize',1)
+    'MarkerSize',1);
 
 
 
@@ -53,27 +53,32 @@ xv=[];
 for i =1:length(binEdges)-1
     xv = [xv,mean(binEdges(i:i+1))];
 end
-hold on
-plot(xv,binMean(1:end),'xk','MarkerSize',15,'LineWidth',2)
-xlim([-60 60])
-ylim([0.6 1])
+hold on;
+h3 = plot(xv,binMean(1:end),'Color',[.4 .4 .4],'Marker','x','MarkerSize',15,'LineWidth',2, 'Linestyle','none');
+xlim([-60 60]);
+ylim([0.6 1]);
 
 clear x y s
 x=-60:1:60;
 for s=1:length(x)
     y(s) = (exp( 1.564707+0.004675*x(s)))/(1+exp(1.564707+0.004675*x(s)));
 end
-plot(x,y,'--k','LineWidth',4,...
+h4 = plot(x,y,'Color',[.4 .4 .4],'LineWidth',2,...
     'MarkerEdgeColor','k',...
     'MarkerFaceColor','k',...
-    'MarkerSize',5)
+    'MarkerSize',1);
 
-xlabel('% signal change','FontSize',25)
-ylabel('Accuracy','FontSize',25)
+xlabel('% signal change','FontSize',25);
+ylabel('Accuracy','FontSize',25);
 %set(gca,'FontSize', 12, 'Box','off');
 %legend('contralateral', '', 'ipsilateral')
-
-set(gca,'FontSize',24)
-set(gca,'linewidth',2);
+legend([h1,h3],'Contralateral','Ipsilateral','Location','Best');
+legend BOXOFF
+set(gca, 'Box','off');
 hold off
-%print(gcf, 'FEF_logit', '-dtiff  ', '-r600')
+set(gcf, 'Color', 'white');
+
+set(gca,'FontSize',24);
+set(gca,'linewidth',2);
+hold off;
+export_fig logit.tiff -painters -m4;
