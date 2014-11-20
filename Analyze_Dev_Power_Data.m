593712b9c8a43b02976471fc3e7f1785bdabff26
kaihwang <kai.hwang@gmail.com>
2014-11-20 14:52:54 -0800

    include all ROIs into one model.

--- Analyze_Dev_Power_Data.m
+++ Analyze_Dev_Power_Data.m
@@ -1,19 +1,102 @@
-% This is a plotting script to analyze the local power task modulation for
-% adult subjects. This was what presented in Figure 3.
+% This is a plotting script to analyze the task modulation of power for each age group.
 % It will make figures showing power spectrum. X is Hz, y is signal change.
 
 % note spectrum power was calculated using MEG_power.m
 %last update 6.13.2014
+% 11.20.2014 edits to address reviewer comments. Specifically. Instead of
+% permuting and testing each ROIs separately. Now we will try to do it in
+% one single model. 
+
+clear all;
+close all;
 
 %load data
 load Dev_Power_Data
 
+%smooth
+spm_smooth(Adult_AS_RFEF,Adult_AS_RFEF,[0,3,0],0);
+spm_smooth(Adult_AS_LFEF,Adult_AS_LFEF,[0,3,0],0);
+spm_smooth(Adult_AS_RDLPFC,Adult_AS_RDLPFC,[0,3,0],0);
+spm_smooth(Adult_AS_RVLPFC,Adult_AS_RVLPFC,[0,3,0],0);
+spm_smooth(Adult_PS_RFEF,Adult_PS_RFEF,[0,3,0],0);
+spm_smooth(Adult_PS_LFEF,Adult_PS_LFEF,[0,3,0],0);
+spm_smooth(Adult_PS_RDLPFC,Adult_PS_RDLPFC,[0,3,0],0);
+spm_smooth(Adult_PS_RVLPFC,Adult_PS_RVLPFC,[0,3,0],0);
+
+spm_smooth(Teen_AS_RFEF,Teen_AS_RFEF,[0,3,0],0);
+spm_smooth(Teen_AS_LFEF,Teen_AS_LFEF,[0,3,0],0);
+spm_smooth(Teen_AS_RDLPFC,Teen_AS_RDLPFC,[0,3,0],0);
+spm_smooth(Teen_AS_RVLPFC,Teen_AS_RVLPFC,[0,3,0],0);
+spm_smooth(Teen_PS_RFEF,Teen_PS_RFEF,[0,3,0],0);
+spm_smooth(Teen_PS_LFEF,Teen_PS_LFEF,[0,3,0],0);
+spm_smooth(Teen_PS_RDLPFC,Teen_PS_RDLPFC,[0,3,0],0);
+spm_smooth(Teen_PS_RVLPFC,Teen_PS_RVLPFC,[0,3,0],0);
+%compile data into single structure for permutation
+%recompile data into the following format ROIxpowerxsubject
+
+
+Adult_AS_Power = [Adult_AS_RFEF, Adult_AS_LFEF, Adult_AS_RDLPFC, Adult_AS_RVLPFC ];
+Adult_AS_Power = reshape(Adult_AS_Power,20,30,4);
+Adult_AS_Power = permute(Adult_AS_Power,[3,2,1]);
+
+Adult_PS_Power = [Adult_PS_RFEF, Adult_PS_LFEF, Adult_PS_RDLPFC, Adult_PS_RVLPFC];
+Adult_PS_Power = reshape(Adult_PS_Power,20,30,4);
+Adult_PS_Power = permute(Adult_PS_Power,[3,2,1]);
+
+Teen_AS_Power = [Teen_AS_RFEF, Teen_AS_LFEF, Teen_AS_RDLPFC, Teen_AS_RVLPFC ];
+Teen_AS_Power = reshape(Teen_AS_Power,17,30,4);
+Teen_AS_Power = permute(Teen_AS_Power,[3,2,1]);
+
+Teen_PS_Power = [Teen_PS_RFEF, Teen_PS_LFEF, Teen_PS_RDLPFC, Teen_PS_RVLPFC];
+Teen_PS_Power = reshape(Teen_PS_Power,17,30,4);
+Teen_PS_Power = permute(Teen_PS_Power,[3,2,1]);
+
+%find the min cluster stat from permutation
+
+nPerm= 5000;
+[~, df, ~, surrog]=statcond({Adult_AS_Power Adult_PS_Power},'mode','perm','naccu',nPerm);
+tVal = icdf('t',0.975,df);
+null_data = reshape(surrog,30,20000);
+
+Null_clusts_mass = zeros(length(null_data),1);
+for n = 1:length(null_data)
+    nd = squeeze(null_data(:,n));
+    null_clusts = bwlabeln(abs(nd)>tVal);
+    null_clust_mass = sum(abs(nd(null_clusts==1)));
+    
+    for j = 2:max(null_clusts)
+        curr_clust_mass = sum(abs(null_data(null_clusts==j)));
+        if curr_clust_mass > null_clust_mass
+            null_clust_mass = curr_clust_mass;
+        end
+    end
+    Null_clusts_mass(n) = null_clust_mass;
+end
+
+clust_stat_threshold = quantile(Null_clusts_mass,1-0.05/8)
+
+[Stats, df, ~, ~]=statcond({Adult_AS_RFEF' Adult_PS_RFEF'},'mode','perm','naccu',nPerm);
+%tVal = icdf('t',0.975,df);
+test_clusts = bwlabeln(abs(Stats)>tVal)
+
+%find the test clusters
+%test_clusts_mass = sum(abs(stats(test_clusts==1)))
+Test_stat_clusts_mass = zeros(max(max(test_clusts)),1);
+for j = 1:max(max(test_clusts))
+    Test_stat_clusts_mass(j) = sum(abs(Stats(test_clusts==j)));
+    %if curr_clust_mass>test_clusts_mass
+    %    test_clusts_mass = curr_clust_mass
+    %end
+end
+Test_stat_clusts_mass
+
 %plot
+
 H=figure('visible','on');
 
 %extract data to plot
-D1=Teen_AS_RVLPFC;
-D2=Teen_PS_RVLPFC;
+D1=Adult_AS_RFEF;
+D2=Adult_PS_RFEF;
 
 %script for time frequency grid smoothing
 % note only for developmental comparison we did smoothing.
@@ -26,18 +109,18 @@ D2=Teen_PS_RVLPFC;
 %     %         D2(:,:,n)= temp;
 % end
 
-H1=shadedErrorBar(FOIs,D1,{@mean,@ste},{'-','LineWidth',4,'Color',rgb('Red')},1);
+H1=shadedErrorBar(FOIs,D1,{@mean,@ste},{'-','LineWidth',4,'Color',rgb('red')},1);
 hold on
-H2=shadedErrorBar(FOIs,D2,{@mean,@ste},{'-','LineWidth',4,'Color',rgb('Blue')},1);
+H2=shadedErrorBar(FOIs,D2,{@mean,@ste},{'-','LineWidth',4,'Color',rgb('blue')},1);
 
 hl=legend([H1.mainLine,H2.mainLine],'Adult AS ','Adult PS','Location','Northeast' );
-ylim([-10 80])
-set(gca,'FontSize',24,'box','off','XGrid','off','YGrid','off')
-set(gcf,'Renderer','openGL')
-axis square
+ylim([-10 80]);
+set(gca,'FontSize',24,'box','off','XGrid','off','YGrid','off');
+set(gcf,'Renderer','openGL');
+axis square;
 
-xlabel('Hz','FontSize',24)
-ylabel('% signal change from baseline','FontSize',24)
+xlabel('Hz','FontSize',24);
+ylabel('% signal change from baseline','FontSize',24);
 set(hl,'FontSize', 24, 'Box','off');
 set(gca,'linewidth',10);
 %set(gca,'xtick',[])
