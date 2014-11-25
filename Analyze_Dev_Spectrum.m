943100d7b95c9a4c6fe0f613778ec9dfdf44c5d0
kaihwang <kai.hwang@gmail.com>
2014-11-25 09:55:23 -0800

    disable plotting.

--- Analyze_Dev_Spectrum.m
+++ Analyze_Dev_Spectrum.m
@@ -93,9 +93,10 @@ Test_stat_clusts_mass
 
 Ps=[];
 for n =1:length(Test_stat_clusts_mass)
-   Ps(n)=1-sum(Test_stat_clusts_mass(n) > Null_clusts_mass)/length(Null_clusts_mass)
+   Ps(n)=1-sum(Test_stat_clusts_mass(n) > Null_clusts_mass)/length(Null_clusts_mass);
     
 end
+Ps
 %Ps > Test_stat_clusts_mass
 
 %[S, C, ~, SM, CP, SP, ~] = MEG_Cluster_Stats_th(D1,D2,1000,.0021); % bonferonni corrected
@@ -105,74 +106,74 @@ end
 
 
 %% plot data
-
-
-
-M1 = mean(D1,3);
-M2 = mean(D2,3);
-
-%FigTitle = [cell2mat(ROIS(r)), ' AS Power'];
-h = figure('visible','on');
-pcolor(CTime,FOIs,M1);caxis([-40 40]);
-%colorbar
-shading flat;
-%title(FigTitle, 'FontSize', 24)
-line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
-line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');close all;
-set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
-xlabel('Time (seconds)','FontSize',24);
-ylabel('Hz','FontSize',24);
-%set(gca,'color','none','yticklabel','')
-%set(gca,'color','none','xticklabel','')
-axis square;
-%set(gca,'color','none')
-set(gca, 'LooseInset', [0,0,0,0]);
-set(gca, 'Color', 'none');
-%export_fig A_RVLPFC.tiff -m4 -transparent
-
-
-%FigTitle = [cell2mat(ROIS(r)), ' PS Power'];;
-h = figure('visible','on');
-pcolor(CTime,FOIs,M2);caxis([-40 40]);
-
-shading flat;
-%title(FigTitle, 'FontSize', 24)
-line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
-line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');close all;
-set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
-xlabel('Time (seconds)','FontSize',24);
-ylabel('Hz','FontSize',24);
-axis square;
-%set(gca,'color','none','yticklabel','')
-%set(gca,'color','none','xticklabel','')
-c=colorbar;
-% Modify Colorbar to a manual setting
-set(c,'location','manual','ActivePositionProperty','OuterPosition');
-hbarPos = get(c,'OuterPosition');
-set(c,'fontsize',24, 'OuterPosition',[hbarPos(1)+0.065 hbarPos(2) hbarPos(3)+0.065 hbarPos(4) ]);
-set(gca, 'LooseInset', [0,0,0,0]);
-set(gca, 'Color', 'none');
-%export_fig T_VLPFC.tiff -m4 -transparent
-
-%FigTitle = ['FEF Correct-InCorrect'];
-h = figure('visible','on');
-pcolor(CTime,FOIs,S.*(SM));caxis([-5 5]);
-colorbar;
-shading flat;
-shading flat;
-%title(FigTitle, 'FontSize', 24)
-line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
-line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');%close all;
-set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
-xlabel('Time (seconds)','FontSize',24);
-ylabel('Hz','FontSize',24);
-axis square;
-c=colorbar;
-set(gca,'color','none');
-set(c,'location','manual','ActivePositionProperty','OuterPosition');
-hbarPos = get(c,'OuterPosition');
-set(c,'fontsize',24, 'OuterPosition',[hbarPos(1)+0.075 hbarPos(2) hbarPos(3)+0.075 hbarPos(4) ]);
-set(gca, 'LooseInset', [0,0,0,0]);
-set(gca, 'Color', 'none');
-%export_fig AvT_RVLPFC.tiff -m4 -transparent
+% 
+% 
+% 
+% M1 = mean(D1,3);
+% M2 = mean(D2,3);
+% 
+% %FigTitle = [cell2mat(ROIS(r)), ' AS Power'];
+% h = figure('visible','on');
+% pcolor(CTime,FOIs,M1);caxis([-40 40]);
+% %colorbar
+% shading flat;
+% %title(FigTitle, 'FontSize', 24)
+% line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
+% line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');close all;
+% set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
+% xlabel('Time (seconds)','FontSize',24);
+% ylabel('Hz','FontSize',24);
+% %set(gca,'color','none','yticklabel','')
+% %set(gca,'color','none','xticklabel','')
+% axis square;
+% %set(gca,'color','none')
+% set(gca, 'LooseInset', [0,0,0,0]);
+% set(gca, 'Color', 'none');
+% %export_fig A_RVLPFC.tiff -m4 -transparent
+% 
+% 
+% %FigTitle = [cell2mat(ROIS(r)), ' PS Power'];;
+% h = figure('visible','on');
+% pcolor(CTime,FOIs,M2);caxis([-40 40]);
+% 
+% shading flat;
+% %title(FigTitle, 'FontSize', 24)
+% line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
+% line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');close all;
+% set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
+% xlabel('Time (seconds)','FontSize',24);
+% ylabel('Hz','FontSize',24);
+% axis square;
+% %set(gca,'color','none','yticklabel','')
+% %set(gca,'color','none','xticklabel','')
+% c=colorbar;
+% % Modify Colorbar to a manual setting
+% set(c,'location','manual','ActivePositionProperty','OuterPosition');
+% hbarPos = get(c,'OuterPosition');
+% set(c,'fontsize',24, 'OuterPosition',[hbarPos(1)+0.065 hbarPos(2) hbarPos(3)+0.065 hbarPos(4) ]);
+% set(gca, 'LooseInset', [0,0,0,0]);
+% set(gca, 'Color', 'none');
+% %export_fig T_VLPFC.tiff -m4 -transparent
+% 
+% %FigTitle = ['FEF Correct-InCorrect'];
+% h = figure('visible','on');
+% pcolor(CTime,FOIs,S.*(SM));caxis([-5 5]);
+% colorbar;
+% shading flat;
+% shading flat;
+% %title(FigTitle, 'FontSize', 24)
+% line([-1.5,-1.5],[2,60],'LineStyle', '-','LineWidth',1.5,'Color','k');
+% line([0,0],[2,60],'LineStyle', '--','LineWidth',1.5,'Color','k');%saveas(h, FigTitle, 'jpg');%close all;
+% set(gca,'FontSize',24,'box','on','XGrid','off','YGrid','off');
+% xlabel('Time (seconds)','FontSize',24);
+% ylabel('Hz','FontSize',24);
+% axis square;
+% c=colorbar;
+% set(gca,'color','none');
+% set(c,'location','manual','ActivePositionProperty','OuterPosition');
+% hbarPos = get(c,'OuterPosition');
+% set(c,'fontsize',24, 'OuterPosition',[hbarPos(1)+0.075 hbarPos(2) hbarPos(3)+0.075 hbarPos(4) ]);
+% set(gca, 'LooseInset', [0,0,0,0]);
+% set(gca, 'Color', 'none');
+% %export_fig AvT_RVLPFC.tiff -m4 -transparent
 
